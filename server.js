eb31d1d7eca85782108dd16f8201a2be614e21dc
gavinlai999 <136124137+gavinlai999@users.noreply.github.com>
2024-02-02 20:59:05 -0800

    es
    yes

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const passingScore = 4;
 
-app.use(bodyParser.json());
-app.use(express.static("./public"));
-
-app.listen(3000, function () {
-    console.log("Server is running on port 3000.");
-});
-
-app.get("/", function (req, res) {
+app.use(express.static(".public"));
+app.get("/", function (req,res){
     res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/style.css");
+});
+app.post("/" , function(req,res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+app.listen(process.env.PORT || 10000, function() {
+    console.log(`Server is running on port ${process.env.PORT}.`);
 });
 
 app.post('/submitQuiz', (req, res) => {
